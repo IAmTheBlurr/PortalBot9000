ab3b14be8a117292ad2ddcc4dfa75abaa788ff2f
Jonathan Craig <blurr@iamtheblurr.com>
2020-10-11 01:19:57 -0700

    Connectors -> Adds stub methods in the Database class
    Getting ready for implementation, modeling out functionality.
    
    Making this class into a context manager too.

--- connectors/database.py
+++ connectors/database.py
@@ -1,8 +1,28 @@
+""" ./connectors/database.py """
 from pymongo import MongoClient
 
-from configurator import Configurator
+from connectors import Configuration
+from entities import Event
 
 
 class Database(MongoClient):
-    def __init__(self, config: Configurator):
+    def __init__(self, config: Configuration):
         super().__init__(config.database_url, config.database_port)
+
+    def __enter__(self):
+        return
+
+    def __exit__(self, *_):
+        return
+
+    def connect(self):
+        return
+
+    def disconnect(self):
+        return
+
+    def update_event(self, event: Event):
+        return
+
+    def write_new_event(self, event: Event):
+        return
